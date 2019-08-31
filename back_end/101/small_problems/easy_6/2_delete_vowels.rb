2a3aa39cc9acb2054a51f825702f78cc58c3aae6
Felicia Bacon <feliciaiveliz@gmail.com>
2019-08-31 18:03:02 -0500

    adding exercise 2

---
+++ back_end/101/small_problems/easy_6/2_delete_vowels.rb
@@ -0,0 +1,31 @@
+=begin
+
+Write a method that takes an array of strings, and returns an array of the same string values, except with the vowels (a, e, i, o, u) removed.
+
+input: array of strings
+output: array of same string
+requirements: 
+- return same string but with vowels deleted
+- vowels: a, e, i, o, u
+- retain case
+data structure: array, string
+algorithm: 
+- create remove_vowels(array)
+- word.gsub(/[^aeiou]/i, '')
+- iterate over array using map
+- return array
+- end method
+=end
+
+def remove_vowels(array)
+  array.map! { |word| word.gsub(/[aeiou]/i, '') }
+end
+
+
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz))
+p remove_vowels(%w(green YELLOW black white)) 
+p remove_vowels(%w(ABC AEIOU XYZ)) 
+
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
