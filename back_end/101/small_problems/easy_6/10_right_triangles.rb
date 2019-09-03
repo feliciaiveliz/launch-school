1cb83823bf0db09a85743fbf04e084a5b0944841
Felicia Bacon <feliciaiveliz@gmail.com>
2019-09-03 14:22:45 -0500

    adding exercise 10

---
+++ back_end/101/small_problems/easy_6/10_right_triangles.rb
@@ -0,0 +1,22 @@
+=begin
+
+Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+input: positive integer
+output: right triangle of stars
+rules: 
+- right triangle- hypotenuse is diagonal side with one end in lower-left and other end in upper-right
+algorithm: 
+- define triangle(num) method
+- num.times {|n| puts (num * " ") + "*" }
+- call .rjust(num) to pad the string with the number of spaces equal to num
+- end method
+=end
+
+def triangle(num)
+  num.times {|n| puts (("*" * n) + "*").rjust(num) }
+end
+
+p triangle(5)
+p triangle(9)
+
