16843d06da1d1c55c7fa3755a88ec94c7abf57ee
Felicia Bacon <feliciaiveliz@gmail.com>
2019-09-15 13:59:26 -0500

    adding exercise 8

---
+++ back_end/100/ruby_basics_exercises/variable_scope/8_value.rb
@@ -0,0 +1,12 @@
+# What will this print and why?
+
+array = [1, 2, 3]
+
+array.each do |element|
+  a = element
+end
+
+puts a
+
+# error message. 'a' is initialize within the block and is reassigned to each element that is passed to the block.
+# Since 'a' wasn't initialized in the outer scope, it is not available to be output.
