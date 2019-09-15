2ae516dfa8762102caeeea29a680bf1077fa0136
Felicia Bacon <feliciaiveliz@gmail.com>
2019-09-15 14:01:42 -0500

    revising exercise 8

--- back_end/100/ruby_basics_exercises/variable_scope/8_value.rb
+++ back_end/100/ruby_basics_exercises/variable_scope/8_value.rb
@@ -8,5 +8,15 @@ end
 
 puts a
 
+# This allows us to output 3.
+array = [1, 2, 3]
+a = nil
+
+array.each do |element|
+  a = element
+end
+
+puts a
+
 # error message. 'a' is initialize within the block and is reassigned to each element that is passed to the block.
 # Since 'a' wasn't initialized in the outer scope, it is not available to be output.
