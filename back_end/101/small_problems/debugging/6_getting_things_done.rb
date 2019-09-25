b685c6edc77c3e205817c4c3e23ecefac540dc9f
Felicia Bacon <feliciaiveliz@gmail.com>
2019-09-25 13:51:45 -0500

    revising exercise 6

--- back_end/101/small_problems/debugging/6_getting_things_done.rb
+++ back_end/101/small_problems/debugging/6_getting_things_done.rb
@@ -37,14 +37,13 @@ Traceback (most recent call last):
 =end
 
 def move(n, from_array, to_array)
-  return if n == 0 || from_array.empty?
+  return if n == 0 || from_array.empty? # this stop execution at zero and prevents it from exceeding its limit
   to_array << from_array.shift
   move(n - 1, from_array, to_array)
 end
 
-# Example
 
-todo = ['study']
+todo = ['study', 'walk the dog', 'coffee with Tom']
 done = ['apply sunscreen', 'go to the beach']
 
 move(2, todo, done)
