21230d583ad8de84be29aa82e229cdd8c5529bb3
Felicia Bacon <feliciaiveliz@gmail.com>
2019-09-25 14:03:20 -0500

    adding exercise 8

--- back_end/101/small_problems/debugging/8_password.rb
+++ back_end/101/small_problems/debugging/8_password.rb
@@ -36,10 +36,10 @@ password = nil
 def set_password
   puts 'What would you like your password to be?'
   new_password = gets.chomp
-  password = new_password
+  new_password
 end
 
-def verify_password
+def verify_password(password)
   puts '** Login **'
   print 'Password: '
   input = gets.chomp
@@ -52,7 +52,7 @@ def verify_password
 end
 
 if !password
-  set_password
+  password = set_password
 end
 
-verify_password
+verify_password(password)
