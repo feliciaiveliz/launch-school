577f651cd4fabd87164c2b10bdff81878dbabd1e
Felicia Bacon <feliciaiveliz@gmail.com>
2019-11-01 11:24:18 -0500

    adding beginnings of codewars exercises

---
+++ codewars.rb
@@ -0,0 +1,34 @@
+ # CODEWARS PRACTICE
+
+
+# 8 KYU
+
+# 1. Grasshopper - Personalized Message by danleavitt0
+
+def greet(name, owner)
+  name == owner ? "Hello boss" : "Hello guest"
+end
+
+# 2. Convert a string to an array by samjam48
+
+def string_to_array(string)
+  string.split
+end
+
+# 3. Keep Hydrated! by paulrichard
+
+def litres(time)
+  (time / 2).floor
+end
+
+# 4. Reversed Strings by jhoffner
+
+def solution(str)
+  str.reverse
+end
+
+# 5. Remove First and Last Character by Steadyx
+
+def remove_char(s) # "string" -> "trin"
+  s[1..-2]                   
+end
