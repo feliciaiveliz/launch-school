ee2c861b50509e65075178ac9ed0dffdbbb78f7f
Felicia Bacon <feliciaiveliz@gmail.com>
2020-01-21 08:50:12 -0600

    .

--- back_end/109/interview_assessment/small_problems/easy_8/1.rb
+++ back_end/109/interview_assessment/small_problems/easy_8/1.rb
@@ -1,17 +1,61 @@
 =begin
 
+Write a method that takes an Array of numbers and then returns the sum of the sums of each leading subsequence for that Array. You may assume that the Array always contains at least one number.
+
 input: an array
 output: integer
 rules: 
 - array will contain at least one number
-- start with the first number
-  - then add the first(sum) + second number
-  - then add that first + second + third, etc
+- return sum of sums of each subsequence of that array
+examples: 
+sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+sum_of_sums([4]) == 4
+sum_of_sums([1, 2, 3, 4, 5]) == 35
 data structure: array, integer
 pseudo-code: 
-- save the first number into a variable called first
-- add the second number to the 'first' variable
-- reassign 'first' to the nex
+- create variable called 'sum' and set it to 0
+- create variable called 'accumulator' and set it to 0
+- iterate over the array, passing each number to the block
+- add the current number to the 'accumulator' and assign it back to 'accumulator'
+- reassign the value of 'accumulator' to 'sum'
+- 'accumulator' is creating the subsequences and holding the sums of each
+- 'sum' is calculating the total sums of all of them together
+- return the total sum
 algorithm: 
+- init. variable 'sum' assign it to 0, sum = 0
+- init. variable 'accumulator', accumulator = 0
+- iterate over array using #each, pass 'number' to the block
+- increment 'accumulator' by the 'number' and assign it back to 'accumulator'
+  - accum. = 1, accum. = 1 + 2, accum. = 1 + (1 + 2) + (1 + 2 + 3), etc.
+- increment 'sum' by 'accumulator', sum += accum.
+  - sum = 1, sum = 4, sum = 11, etc.
+- return 'sum'
 =end
+
+def sum_of_sums(array)
+  accumulator = 0
+  sum = 0
+
+  array.each do |number|
+    accumulator += number
+    sum += accumulator
+  end
+  sum
+end
+
+# or 
+
+def sum_of_sums(array)
+  sum = 0
+  1.upto(array.size) do |current_size|
+    sum += array.slice(0, current_size).inject(:+)
+  end
+  sum
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
 
--- back_end/109/interview_assessment/small_problems/easy_8/2.rb
+++ back_end/109/interview_assessment/small_problems/easy_8/2.rb
@@ -1,8 +1,39 @@
 =begin
 
-input: 
-output: 
+Mad libs are a simple game where you create a story template with blanks for words. You, or another player, then construct a list of words and place them into the story, creating an often silly or funny story as a result.
+
+Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
+
+input: strings
+output: single string sentence
 rules: 
-data structure: 
+- ask for noun, verb, adjective, adverb
+- insert these words into a story sentence
+data structure: string
 pseudo-code: 
+- ask user for noun, verb, adj., adverb
+- save these strings into variables
+- create a sentence that will hold these variables 
+- output this string to the user that uses their words to tell a story
 algorithm: 
+- puts a statement to user that asks for 4 things
+- init. variables and set it to what the user inputs
+- use #puts to output the value of the string sentence
+- use string interpolation to integrate user's choices into sentence
+=end
+
+puts "Enter a noun: "
+noun = gets.chomp
+
+puts "Enter a verb: "
+verb = gets.chomp
+
+puts "Enter an adjective: "
+adjective = gets.chomp
+
+puts "Enter an adverb: "
+adverb = gets.chomp
+
+puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+
+
