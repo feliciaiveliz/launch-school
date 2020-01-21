92bd0df6b094fcfb02f26f3ec2cb59d400b82531
Felicia Bacon <feliciaiveliz@gmail.com>
2020-01-21 07:29:25 -0600

    adding exercises for easy 7

---
+++ back_end/109/interview_assessment/small_problems/easy_8/1.rb
@@ -0,0 +1,17 @@
+=begin
+
+input: an array
+output: integer
+rules: 
+- array will contain at least one number
+- start with the first number
+  - then add the first(sum) + second number
+  - then add that first + second + third, etc
+data structure: array, integer
+pseudo-code: 
+- save the first number into a variable called first
+- add the second number to the 'first' variable
+- reassign 'first' to the nex
+algorithm: 
+=end
+
