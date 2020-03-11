236268b46ee00660a35ae03eae07c083070f49d7
Felicia Bacon <feliciaiveliz@gmail.com>
2020-03-11 11:48:31 -0500

    .

--- back_end/109/interview_assessment/small_problems/march_10.rb
+++ back_end/109/interview_assessment/small_problems/march_10.rb
@@ -1,32 +1,32 @@
-# def repeat(string, n)
-#   n.times { puts string }
-# end
+def repeat(string, n)
+  n.times { puts string }
+end
 
-# p repeat('Hello', 3)
+p repeat('Hello', 3)
 
-# def is_odd?(n)
-#   n % 2 != 0
-# end
+def is_odd?(n)
+  n % 2 != 0
+end
 
-# def is_odd?(n)
-#   n.abs.remainder(2) == 1
-# end
+def is_odd?(n)
+  n.abs.remainder(2) == 1
+end
 
-# p puts is_odd?(2)    # => false
-# p puts is_odd?(5)    # => true
-# p puts is_odd?(-17)  # => true
-# p puts is_odd?(-8)   # => false
-# p puts is_odd?(0)    # => false
-# p puts is_odd?(7)    # => true
+p puts is_odd?(2)    # => false
+p puts is_odd?(5)    # => true
+p puts is_odd?(-17)  # => true
+p puts is_odd?(-8)   # => false
+p puts is_odd?(0)    # => false
+p puts is_odd?(7)    # => true
 
-# def digit_list(number)
-#   number.digits.reverse
-# end
+def digit_list(number)
+  number.digits.reverse
+end
 
-# puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
-# puts digit_list(7) == [7]                     # => true
-# puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
-# puts digit_list(444) == [4, 4, 4]             # => true
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
 
 vehicles = [
   'car', 'car', 'truck', 'car', 'SUV', 'truck',
