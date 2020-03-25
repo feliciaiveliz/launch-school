b12b5c429058d782a0cf556870987e4c065fa44c
Felicia Bacon <feliciaiveliz@gmail.com>
2020-03-25 16:22:21 -0500

    .

--- back_end/109/interview_assessment/codewars/march_25.rb
+++ back_end/109/interview_assessment/codewars/march_25.rb
@@ -77,4 +77,279 @@ def alphabet_position(string)
   result.join(' ')
 end
 
+def alphabet_position(text)
+  alphabet = ('a'..'z').to_a
+  text.delete!('^a-zA-Z')
+  text.chars.map { |letter| (alphabet.index(letter.downcase) + 1) }.join(' ')
+end
+
 p alphabet_position("The sunset sets at twelve o' clock.") == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+# Simple Pig Latin | 6 kyu
+
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+=begin
+
+input: string of words and punct.
+output: string of words and punct.: add first letter to end of word + ay
+problem:
+- iterate over string, given word
+  - remove first letter and add to end
+  - add 'ay'
+  - add punct. as is, without adding 'ay'
+ALGORITHM:
+- initialize 'result' to []
+- split string using #split, given word
+  - IF word is not 'letters'
+    - result << word
+  - ELSE 
+    - split word using #chars:
+      - use word.insert(-1, word.chars.shift) + 'ay'
+      - push the whole word to 'result'
+- return join
+=end
+
+def pig_it(string)
+  punct = %w(? ! . , ' ")
+  result = []
+  string = string.split
+  string.each do |word|
+    if punct.include?(word)
+      result << word
+    else
+     word = word.chars
+     word.insert(-1, word.shift) << 'ay'
+     result << word.join
+    end
+  end
+  result.join(' ')
+end
+
+p pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+p pig_it('Hello world !')     # elloHay orldway !
+
+# Given a number n, return the number of positive odd numbers below n, EASY!
+
+=begin
+
+input: integer
+output: array of odd numbers below 'n'
+problem:
+- create a range from 1 to (one less than) 'n'
+- use #count to grab odd numbers
+- return array of odd numbers
+ALGORITHM:
+- create range: (1...number)
+- #count: if number is odd, choose number
+- return array
+=end
+
+def odd_count(number)
+  (1...number).count { |n| n.odd? }
+end
+
+p odd_count(7) == 3 #i.e [1, 3, 5]
+p odd_count(15) == 7 #i.e [1, 3, 5, 7, 9, 11, 13]
+
+# The year of 2013 is the first year after the old 1987 with only distinct digits.
+
+# Now your task is to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+
+# Input/Output
+# [input] integer year
+
+# 1000 ≤ year ≤ 9000
+
+# [output] an integer
+
+# the minimum year number that is strictly larger than the input number year and all its digits are distinct.
+
+=begin
+
+input: integer year
+output: integer year with distinct year
+problem:
+- increment 'year' by 1 until
+  - year.digits.uniq is equal to year.digits
+- if the year numbers are uniq then return the year
+=end
+
+def distinct_digit_year(year)
+  loop do 
+    year += 1
+    break if year.digits == year.digits.uniq
+  end
+  year
+end
+  
+p distinct_digit_year(1987) #== 2013
+p distinct_digit_year(2013) #== 2014
+p distinct_digit_year(2229) #== 2301
+
+# Given an array of integers, Find the minimum sum which is obtained from summing each Two integers product.
+
+# Array/list will contain positives only.
+# Array/list will always has even size.
+  
+=begin
+
+input: array of integers
+output: integer, minimum sum
+problem:
+- sort array from smallest to largest number
+- remove first and last numbers from arry until its empty
+  - multiply each two numbers and add to array
+  - sum the products
+ALGORITHM:
+- initilaize 'result' to []
+- sort the array using #sort!
+- until array is empty:
+  - use #shift and #pop and multiply them
+  - add the result to 'result'
+- sum the result array
+=end
+
+def min_sum(array)
+  result = []
+  array.sort!
+  
+  until array.empty?
+    result << array.shift * array.pop
+  end
+  result.sum
+end
+p min_sum([5,4,2,3]) == 22
+p min_sum([12,6,10,26,3,24]) == 342
+
+
+
+# Given a number, return the maximum number that could be formed from the digits of the number given.
+
+# Only Natural numbers passed to the function , numbers Contain digits [0:9] inclusive.
+
+# Digit Duplications could occur , So also consider it when forming the Largest
+
+=begin
+
+input: integer
+output: maxixum integer
+proble:
+- sort number from largest to smallest
+- return the number
+ALGORITM:
+- use #digits and #sort to get array of largest to smallest digits
+- use #join and #to_i to convert back to integer
+=end
+
+def max_number(number)
+  number.digits.sort.reverse.join.to_i
+end
+p max_number(213) == 321
+p max_number(7389) == 9873
+p max_number(63792) == 97632
+
+# Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime'.
+
+=begin
+
+input: integer
+output: array of divisors of integer, or string "N is prime"
+problem:
+- create a range from 2 to (one less than) number
+- iterate and select numbers that are evenly divided into number
+- return array
+- if the array is empty, return 'number is prime'
+ALGORITHM:
+- create range from 2...number 
+- initialize 'result' to:
+  - use #select and iterate with 'n'
+    - select n if number mod n is == to 0
+- return string if array is empty?
+=end
+
+def divisors(number)
+  result = (2...number).select { |n| number % n == 0 }
+  result.empty? ? "#{number} is prime" : result
+end
+
+p divisors(12) # should return [2,3,4,6]
+p divisors(25) # should return [5]
+p divisors(13) # should return "13 is prime"
+
+# Complete the solution so that the function will break up camel casing, using a space between words.
+  
+=begin
+input: string
+output: string, w/ space in front of capital letter
+problem:
+- iterate through string and add leters to a new string
+- if current letter is capital, add " " first, then current letter
+- return string
+ALGOIRTHM:
+- initialize 'rsult' to ""
+- split string using #chars and iterate:
+- add letter to string if string if lowercase
+- add " " then letter if uppercase
+- return string
+=end
+
+def solution(string)
+  result = ""
+  
+  string.each_char do |char|
+    if char == char.upcase
+      result << " " << char
+    else
+      result << char
+    end
+  end
+  result
+end
+
+p solution("camelCasing")  #==  "camel Casing"
+
+# The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))((" 
+
+=begin
+input: string
+output: string, if char count is 1, "(", if char count is > 1, ")"
+problem:
+- initilaize 'result' to ""
+- downcase string using #downcase!
+- split string using #each_char, given char
+  - IF string count of char is == 1
+    - add "(" to result
+  - ELSE 
+    - ")" to result
+  - END
+- return result
+=end
+
+def duplicate_encode(string)
+  string.downcase!
+  result = ""
+  string.each_char do |char|
+    if string.count(char) == 1
+      result << "("
+    else
+      result << ")"
+    end
+  end
+  result
+end
+
+def duplicate_encode(string)
+  string.downcase!
+  string.chars.map { |char| string.count(char) > 1 ? ")" : "(" }.join
+end
+
+p duplicate_encode("din") == "((("
+p duplicate_encode("recede") == "()()()"
+p duplicate_encode("Success") == ")())())"
+p duplicate_encode("(( @") == "))(("
