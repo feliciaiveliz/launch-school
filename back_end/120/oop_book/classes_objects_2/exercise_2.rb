b31ab7de238520e2a2ad3639fa3201cff9e32c5f
Felicia Bacon <feliciaiveliz@gmail.com>
2020-04-08 16:15:13 -0500

    adding exercises for 2nd chapter

--- back_end/120/oop_book/classes_objects_2/exercise_2.rb
+++ back_end/120/oop_book/classes_objects_2/exercise_2.rb
@@ -10,6 +10,6 @@ end
 
 bob = Person.new("Steve")
 bob.name = "Bob"
-puts self.name
+puts bob.name
 # We are trying to change the name bob but have no created the method to do so. 
 # attr_reader only gives us the getter method and not the setter method, which is needed to make a change to an instance variable
