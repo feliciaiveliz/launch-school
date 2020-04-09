a9ebb26542fb64d95ffdfc0d8e7ed3bcd681e425
Felicia Bacon <feliciaiveliz@gmail.com>
2020-04-09 12:09:01 -0500

    adding exercise 1

---
+++ back_end/120/oop_exercises/classes_objects_1/1.rb
@@ -0,0 +1,10 @@
+# Find the Class
+
+# Update the following code so that, instead of printing the values, each statement prints the name of the class to which it belongs.
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class
+
+# All values are objects. Each object is an instance of a class.
+# Use `class` to find the class to which the object belongs to.
