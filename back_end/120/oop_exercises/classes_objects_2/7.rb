a9badddb4fc06073e50627403364c164e066ee13
Felicia Bacon <feliciaiveliz@gmail.com>
2020-04-13 12:15:20 -0500

    adding exercises

---
+++ back_end/120/oop_exercises/classes_objects_2/7.rb
@@ -0,0 +1,32 @@
+# Identify Yourself (Part 2)
+
+# Update the following code so that it prints I'm Sophie! when it invokes puts kitty.
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    puts "I'm #{self.name}!"
+  end
+end
+
+kitty = Cat.new("Sophie")
+puts kitty
+
+# Launc School
+
+class Cat
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "I'm #{name}!"
+  end
+end
+
+kitty = Cat.new("Sophie")
+puts kitty
---
+++ back_end/120/oop_exercises/classes_objects_2/8.rb
@@ -0,0 +1,24 @@
+# Public Secret
+
+# Using the following code, create a class named Person with an instance variable named @secret. Use a setter method to add a value to @secret, then use a getter method to print @secret.
+
+class Person
+  attr_accessor :secret
+end
+
+person1 = Person.new
+person1.secret = "Shh.. this is a secret!"
+puts person1.secret
+
+# or
+
+class Person
+  def secret
+    @secret
+  end
+
+  def secret=(value)
+    @secret = value
+  end
+end
+
