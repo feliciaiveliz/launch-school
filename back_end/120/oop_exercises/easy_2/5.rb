510f888739bbeeba8c3c2f813b0363ead42778d3
Felicia Bacon <feliciaiveliz@gmail.com>
2020-04-19 13:44:36 -0500

    adding exercises

---
+++ back_end/120/oop_exercises/easy_2/5.rb
@@ -0,0 +1,20 @@
+# What Will This Do?
+
+class Something
+  def initialize
+    @data = 'Hello'
+  end
+
+  def dupdata
+    @data + @data
+  end
+
+  def self.dupdata
+    'ByeBye'
+  end
+end
+
+thing = Something.new
+puts Something.dupdata # 'ByeBye'
+puts thing.dupdata # 'HelloHello'
+
