a320d626e82fe138d4bea7433c731459708015f1
Felicia Bacon <feliciaiveliz@gmail.com>
2020-05-08 19:08:50 -0500

    adding medium 1 exercises and daily log

---
+++ back_end/120/daily_log/may_8.md
@@ -0,0 +1,19 @@
+### May 8
+
+**Work Done**: 
+- Finished Debugging exerises
+- Started Medium 1 exercises
+
+**Goals for Tomorrow**:
+- Review anki cards
+- finish LS exercises (accessor methods, easy 1/2, medium 1)
+- finish notes in lesson 2/3/4
+  - Lesson 2: Accessor methods, collaborator objects, modules, crc cards
+  - Lesson 3: Fake operators, exceptions
+  - Lesson 4: Hard 1
+- Take lesson 5 quiz
+
+**Time Spent**: 4 hours
+
+**Goal Met**? 
+- [x] (4 hours)
---
+++ back_end/120/oop_exercises/medium_1/2.rb
@@ -0,0 +1,81 @@
+# A fixed-length array is an array that always has a fixed number of elements. Write a class that implements a fixed-length array, and provides the necessary methods to support the following code:
+
+# mine
+class FixedArray
+  attr_reader :array
+
+  def initialize(size)
+    @size = size
+    @array = [nil] * @size
+  end
+
+  def []=(index, element)
+    index_out_of_range?(index, array.size)
+    array[index] = element
+  end
+
+  def [](index)
+    index_out_of_range?(index, array.size)
+    array[index]
+  end
+
+  def to_a
+    array.clone
+  end
+
+  def to_s
+    "#{array}"
+  end
+
+  private
+
+  def index_out_of_range?(index, size)
+    raise IndexError if index >= array.size
+  end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
