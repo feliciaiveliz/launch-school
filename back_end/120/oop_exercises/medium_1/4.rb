717dd845c6506ec9ce877841d2cd1c3d5977257f
feliciaiveliz <feliciaiveliz@gmail.com>
2020-05-23 14:49:19 -0500

    adding exercises

---
+++ back_end/120/oop_exercises/medium_1/4.rb
@@ -0,0 +1,85 @@
+# Your task is to write a CircularQueue class that implements a circular queue for arbitrary objects. The class should obtain the buffer size with an argument provided to CircularQueue::new, and should provide the following methods:
+# enqueue to add an object to the queue
+# dequeue to remove (and return) the oldest object in the queue. It should return nil if the queue is empty.
+
+=begin
+
+input: integer, buffer size
+output: boolean; true
+rules: 
+- enque: add item to queue
+- dequeue: remove and return oldest item
+- return nil if queue is empty
+algo:
+- create CircularQueue class
+- pass in integer parameter: buffer size to #initialize
+=end
+
+class CircularQueue
+  def initialize(size)
+    @buffer = [nil] * size
+    @next_position = 0
+    @oldest_position = 0
+  end
+
+  def enqueue(object)
+    unless @buffer[@next_position].nil?
+      @oldest_position = increment(@next_position)
+    end
+
+    @buffer[@next_position] = object
+    @next_position = increment(@next_position)
+  end
+
+  def dequeue
+    value = @buffer[@oldest_position]
+    @buffer[@oldest_position] = nil
+    @oldest_position = increment(@oldest_position) unless value.nil?
+    value
+  end
+
+  private
+
+  def increment(position)
+    (position + 1) % @buffer.size
+  end
+end
+
+queue = CircularQueue.new(3)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
+
+queue = CircularQueue.new(4)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 4
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
