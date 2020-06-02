ea84ed8742d4c644fd04b636cd8a04f8b5c28113
feliciaiveliz <feliciaiveliz@gmail.com>
2020-06-01 19:11:34 -0500

    adding practice problem

--- back_end/129/practice.rb
+++ back_end/129/practice.rb
@@ -40,3 +40,41 @@ sa_zoo << ellie << fluffy
 
 p sa_zoo.total_animals
 p Zoo.total_animals
+
+class House
+  def self.info
+    "#{self}"
+  end
+  
+  def lovelyvilla
+    "#{self.name}"
+  end
+end
+
+class Cottage < House
+  attr_accessor :name
+  
+  def initialize(n)
+    self.name = n
+  end
+  
+  def info
+    "I am #{name}"
+  end
+  
+  def rent
+    "I can be rented"
+  end
+end
+
+class Lakeside < Cottage
+  def describe
+    "Location: #{self.class.info}; Name: #{lovelyvilla}."
+  end
+end
+
+lovelyvilla = Lakeside.new('Lovely Villa')
+
+p lovelyvilla.describe
+
+# "Location: Lakeside; Name: Lovely Villa"
