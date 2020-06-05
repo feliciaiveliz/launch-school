2344c712db96b6215acfa7b9630b6d7010e3d4cb
feliciaiveliz <feliciaiveliz@gmail.com>
2020-06-05 14:52:22 -0500

    adding each method

---
+++ back_end/130/lesson_1/each.rb
@@ -0,0 +1,16 @@
+# [1, 2, 3].each { |num| puts num } # outputs 1 2 and 3 and returns the original calling object; [1, 2, 3]. This is so that developers can chain methods onto the return value
+
+def each(array)
+  counter = 0
+
+  while counter < array.size
+    yield(array[counter])
+    counter += 1
+  end
+
+  array
+end
+
+each([1, 2, 3, 4, 5]) do |num|
+  puts num
+end
