6ada99298381298a968d42faaa2bd5b4597a0837
feliciaiveliz <feliciaiveliz@gmail.com>
2020-08-06 19:16:50 -0500

    adding odd words problem

---
+++ back_end/139/challenges/easy_2/odd_words.rb
@@ -0,0 +1,44 @@
+=begin
+
+input: string
+output: new string
+problem:
+- returned string should contain words seperated by a single space
+- last word will end with a period
+- odd words will be reversed; even words will be copied as is
+- remove all extra spaces and punctuation
+- reverse the odd words at odd indexed places
+algorithm:
+- delete anything from the string that is not a letter and spaces
+- remove duplicate spaces using #strip
+- save this new string into 'clean_words'
+- split string into individual words using #split
+- 'clean_words' = iterate using #map and #with_index, given 'word' and 'idx'
+  - reverse 'word' if 'idx' odd?
+- convert with #join(' ')
+- << '.' on returned string if @sentence[-1] == '.'
+- otherwise return clean_words
+=end
+
+class OddWords
+  def initialize(sentence)
+    @sentence = sentence
+  end
+
+  def reverse!
+    clean_words = @sentence.delete('^a-z ').strip
+    clean_words = clean_words.split.map.with_index do |word, idx|
+      word = word.reverse if idx.odd?
+      word
+    end.join(' ')
+    @sentence[-1] == '.' ? clean_words << '.' : clean_words
+  end
+end
+
+p OddWords.new('').reverse! == ''
+p OddWords.new('.').reverse! == '.'
+p OddWords.new(' pizza.').reverse! #== 'pizza.'
+p OddWords.new('whats the matter with kansas').reverse! #== 'whats eht matter htiw kansas'
+p OddWords.new('i rather  hang out with. my  cat  .').reverse! #== 'i rehtar hang tuo with ym cat.'
+p OddWords.new('felicia  iveliz   khan  bacon').reverse! #== 'felicia zilevi khan nocab'
+
---
+++ back_end/139/challenges/easy_2/odd_words_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require_relative 'odd_words'
+
+class OddWordsTest < Minitest::Test
+  def test_empty_string
+    assert_equal('', OddWords.new('').reverse!)
+  end
+
+  def test_period
+    assert_equal('.', OddWords.new('.').reverse!)
+  end
+
+  def test_single_word
+    assert_equal('pizza.', OddWords.new(' pizza.').reverse!)
+  end
+
+  def test_single_spaces_between_all_words
+    expected = 'whats eht matter htiw kansas'
+    assert_equal(expected, OddWords.new('whats the matter with kansas').reverse!)
+  end
+
+  def test_random_amounts_of_spaces_between_words
+    expected = 'i rehtar hang tuo with ym cat.'
+    assert_equal(expected, OddWords.new('i rather  hang out with. my  cat  .').reverse!)
+  end
+
+  def test_whole_name
+    expected = 'felicia zilevi khan nocab'
+    assert_equal(expected, OddWords.new('felicia  iveliz   khan  bacon').reverse!)
+  end
+end
+
