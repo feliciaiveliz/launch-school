02456781260333d67f6929f4d02b3338c93e1f01
feliciaiveliz <feliciaiveliz@gmail.com>
2020-08-07 12:16:47 -0500

    .

--- back_end/139/challenges/easy_1/anagram.rb
+++ back_end/139/challenges/easy_1/anagram.rb
@@ -1,10 +1,34 @@
 =begin
-input: array of words; single string word
-output: array of word(s)
+
+input: string word; array of string words
+output: array of words that are anagrams of input word
+problem:
+- sort each word and determine if it is equal to the input string sorted
+- if it is, add it to the results array
 algorithm:
-- save word to '@word'
-- 
+- save a copy of input word to 'word_copy'
+- initialize 'anagrams' to []
+- iterate over words, given 'word':
+  - downcase the word
+  - add to 'anagrams' if word.chars.sort == 'input' word.chars.sort 
+    - and if 'word' != to 'input_word'
+- return anagrams
 =end
+
+class Anagram
+  def initialize(string)
+    @string = string.downcase.chars.sort
+    @string_copy = string.clone
+  end
+
+  def match(words)
+    words.select do |word|
+      word = word.downcase
+      sorted_word = word.downcase.chars.sort
+      sorted_word == @string && word != @string_copy
+    end
+  end
+end
 
 class Anagram
   def initialize(word)
