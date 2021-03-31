401b5a82b10f44ceb6254c047148a9b28afb927d
Felicia Bacon <feliciaiveliz@gmail.com>
2021-03-31 12:29:21 -0500

    Moving exercises and adding new JS exercises

---
+++ front_end/JS210/practice_problems-3.js
@@ -0,0 +1,60 @@
+// let apples = 3;
+// let bananas = 5;
+//
+// if (apples == bananas) {
+//   console.log("Equal!");
+// }
+
+// let apples = 3;
+// let bananas = '3';
+//
+// if (apples == bananas) {
+//   console.log("Equal!");
+// }
+
+// let apples = 3;
+// let bananas = '3';
+//
+// if (apples === bananas) {
+//   console.log("Equal!");
+// }
+//
+// // The === operator only evaluates to true if they are equal and of the same type
+
+// let apples = 3;
+// let bananas = '3';
+//
+// if (apples === bananas) {
+//   console.log("Equal!");
+// } else if (apples == bananas) {
+//   console.log("The numbers are the same, but just not the same type!")
+// } else {
+//   console.log("Not equal.");
+// }
+
+// let apples = 3;
+// let bananas = '3';
+//
+// if (apples === bananas) {
+//   console.log("Equal!");
+// } else {
+//     if (apples == bananas) {
+//       console.log("The numbers are the same, but just not the same type!")
+//     }
+//     else {
+//       console.log("Not equal.");
+//     }
+// }
+
+// let apples = 3;
+// let bananas = 3;
+// let areEqual = apples === bananas;
+// console.log(areEqual);
+//
+// let apples = 3;
+// let bananas = undefined;
+// let eitherOr = apples || bananas;
+// console.log(eitherOr);
+
+let lastName = "Bacon";
+let familyMessage = lastName === "Bacon" ? "You're part of the family!" : "You're not family!";
